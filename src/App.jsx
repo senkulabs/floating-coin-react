0aad5b90b17ae782f32bcf65fdf33d4eac010983
Kresna Satya <kresnasatya72@gmail.com>
2025-01-17 16:28:47 +0800

    feat: add buy chocolate button

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import Coin from './Coin'
 import styles from './App.module.css'
 import FloatingText from './FloatingText'
 
+const CHOCOLATE_COST = 9 
+
 function App() {
   const [numOfCoins, setNumOfCoins] = useState(0)
+  const [numOfChocolate, setNumOfChocolate] = useState(0)
+
+  function buyChocolate() {
+    setNumOfCoins(numOfCoins - CHOCOLATE_COST);
+    setNumOfChocolate(numOfChocolate + 1);
+  }
 
   function handleNumOfCoins () {
     setNumOfCoins(numOfCoins + 2); 
@@ -19,6 +27,10 @@ function App() {
             +2
           </FloatingText>
         </div> }
+        <button disabled={numOfCoins < CHOCOLATE_COST} className={styles.shopItem}
+        onClick={buyChocolate}>
+          Buy chocolate {numOfChocolate > 0 && (`(${numOfChocolate})`)}
+        </button>
       </main>
       <footer>
         Your coin balance:
