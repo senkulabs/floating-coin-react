24d8e8bed34c089fe34aa13b213761b4ed24b2b9
Kresna Satya <kresnasatya72@gmail.com>
2025-01-17 14:03:55 +0800

    feat: update App component + state of numOfCoins

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './Coin.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numOfCoins, setNumOfCoins] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="wrapper">
+      <main>
+        <div className="coin-wrapper">
+          <button className="coin" onClick={() => setNumOfCoins(numOfCoins + 2)}>
+            <span className="visually-hidden">Add 2 coin</span>
+            <img className="coin-image" alt="" src="https://sandpack-bundler.vercel.app/img/toonie.png" />
+          </button>
+        </div>
+      </main>
+      <footer>
+        Your coin balance:
+        <strong>{numOfCoins}</strong>
+      </footer>
+    </div>
   )
 }
 
